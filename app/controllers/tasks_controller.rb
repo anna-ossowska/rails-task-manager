caed0edba282d9bc19ec937324125c53ff5dac72
Anna Ossowska <ossowska.anna1@gmail.com>
2020-09-22 14:46:06 +0200

    WIP

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,20 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def task_params
+    # strong params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
