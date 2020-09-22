6f2fc3509b04f66f1e0a1cfdf68554683041263f
Anna Ossowska <ossowska.anna1@gmail.com>
2020-09-22 16:12:48 +0200

    The app is working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  
   def index
     @tasks =Task.all
   end
@@ -17,9 +18,27 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path(@task)
+  end
+
+  private
+
   def task_params
     # strong params
     params.require(:task).permit(:title, :details, :completed)
   end
-
 end
+
